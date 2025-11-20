25c52ea5be99be3cce56aebb451a4f9f7b6578c8
adsoftsito <adsoft@live.com.mx>
2025-11-20 17:27:26 +0000

    add hello

---
+++ hello/main.py
@@ -0,0 +1,9 @@
+from antlr4 import *
+from HelloLexer import HelloLexer
+from HelloParser import HelloParser
+input_text = input("> ")
+lexer = HelloLexer(InputStream(input_text))
+stream = CommonTokenStream(lexer)
+parser = HelloParser(stream)
+tree = parser.r()
+print(tree.toStringTree(recog=parser))
